f146035fedc45d6d65e45d03ff14eafeaec0c02c
Gorbunov Vladislav <wint-mil@mail.ru>
2026-06-10 12:58:12 +0300

    Add cor lib

---
+++ vocabulary-libs/vocabulary-lib-cor/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    id("build-jvm")
+}
+
+dependencies {
+    implementation(kotlin("stdlib-jdk8"))
+    implementation(libs.coroutines.core)
+
+    implementation(kotlin("test-junit"))
+    implementation(libs.coroutines.test)
+}
